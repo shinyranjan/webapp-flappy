9928e3c01540545ed003e7694d24dc2b35555ff5
V3511 (Conference ID) <v3511@cam.ac.uk>
2015-07-22 17:19:06 +0100

    Day 3

--- js/flappy.js
+++ js/flappy.js
@@ -12,8 +12,9 @@ var player;
 var pipes = [];
 // the interval (in seconds) at which new pipe columns are spawned
 var pipeInterval = 1.75;
-
 var track;
+var stars = [];
+var background;
 
 //jQuery("#fullName").on("keyup", function(event_details) {
 //    console.log($("#fullName").val());
@@ -26,8 +27,9 @@ $("#greeting-form").on("submit", function(event_details) {
     var greeting_message = greeting + name + " " + score;
     $("#greeting-form").fadeOut(500);
     $("#greeting").append("<p>" + greeting_message + "</p>");
-    $("#greeting").delay(3000);
-    $("#greeting").fadeOut(2000);
+    $("#greeting").delay(2000);
+    $("#greeting").fadeOut(1000);
+    //game.state.restart(); Don't use otherwise redirected to "success" page
     //$("#greeting").append("<p>" + greeting_message + "</p>");
     $.post("/score", {
         "fullName": name,
@@ -49,16 +51,39 @@ function preload() {
     // make backgrond image file available to game and associate with alias backgroundImg
     game.load.image("backgroundImg", "../assets/Deathstar.gif");
     game.load.audio("soundtrack", "../assets/Starwars.mp3");
+    game.load.image("splashDisplayImg", "../assets/Gamestart.gif");
+    game.load.image("star","../assets/star.png");
+    game.load.image("go","../assets/Gameover.png");
 }
 
 function create() {
+    game.started = false;
+
+    var splashDisplay = game.add.image(0, 0, "splashDisplayImg");
+    splashDisplay.width = 790;
+    splashDisplay.height = 400;
+
+    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(start);
+}
+
+function start() {
+
+    if (game.started) {
+        return;
+    }
+
+    game.started = true;
+
     //background image creation
-    var background = game.add.image(0, 0, "backgroundImg");
+
+
+    background = game.add.image(0, 0, "backgroundImg");
     background.width = 790;
     background.height = 400;
     // set the background colour of the scene(if you have a background image, color won't be seen)
     game.stage.setBackgroundColor("#F3D3A3");
     // add welcome text
+
     // game.add.text(20, 20, "Flappy Doge",
     //{font: "30px Arial", fill: "#FFFFFF"});
     // add score text
@@ -75,18 +100,43 @@ function create() {
     player.body.gravity.y = 400;
     // associate spacebar with jump function
     game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(playerJump);
+    game.input.keyboard.addKey(Phaser.Keyboard.P).onDown.add(pause);
+    game.input.keyboard.addKey(Phaser.Keyboard.U).onDown.add(unPause);
     // time loop for game to update
     game.time.events.loop(pipeInterval * Phaser.Timer.SECOND, generatePipe);
     track = game.sound.play("soundtrack");
     player.anchor.setTo(0.5, 0.5);
 }
+
+function pause() {
+    game.paused = true;
+}
+
+function unPause() {
+    game.paused = false;
+}
+
 // This function updates the scene. It is called for every new frame.
 function update() {
+
+
+    if (player == undefined) {
+        return;
+    }
 
     if(player.body.y < 0 || player.body.y > 400){
         gameOver();
     }
     // Call gameOver function when player overlaps with any pipe
+
+
+    for(var i=stars.length - 1; i>=0; i--){
+        game.physics.arcade.overlap(player,stars[i], function(){
+            stars[i].destroy();
+            stars.splice(i,1);
+            changeScore();
+        });
+    }
 
     game.physics.arcade
         .overlap(player,
@@ -113,6 +163,22 @@ function addPipeBlock(x, y) {
     block.body.velocity.x = -200;
 }
 
+function addStar(x, y) {
+    // make a new pipe block
+    var block = game.add.sprite(x,y,"star");
+    block.scale.x = 0.001;
+    block.scale.y = 1.5;
+    //block.transparent = false;
+    // insert it in the pipe array
+    stars.push(block);
+    // enable physics engine for the block
+    game.physics.arcade.enable(block);
+    // set the block's horizontal velocity to a negative value
+    // (negative x value for velocity means movement will be towards left)
+    block.body.velocity.x = -200;
+    block.alpha = 1.0;
+}
+
 // Generate moving pipe
 function generatePipe() {
     // Generate  random integer between 1 and 5. This is the location of the
@@ -127,7 +193,7 @@ function generatePipe() {
         }
     }
     // Increment the score each time a new pipe is generated.
-    changeScore();
+    addStar(750,(gapStart * 50) + 25);
 }
 
 function playerJump() {
@@ -146,11 +212,20 @@ function changeScore() {
 function gameOver() {
     // stop the game (update() function no longer called)
     //game.destroy();
+    //background.hide();
+    var background2 = game.add.image(0, 0, "go");
+    background2.width = 790;
+    background2.height = 400;
+
     $("#score").val(score.toString());
+    game.paused = true;
     $("#greeting").show();
     track.stop();
-    game.state.restart();
-
+    //game.state.restart();
+    stars = [];
+    //game.input.keyboard.addKey(Phaser.Keyboard.ENTER).onDown.add(function() {
+    //    create();
+    //});
 }
 
 function refreshScores() {
